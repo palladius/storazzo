86b11863c54370dfc58e40c7b842e3c8fc563153
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-07-30 18:22:28 +0200

    Bump storazzo to 0.2.3._TODOFileRead.1

--- lib/storazzo/version.rb
+++ lib/storazzo/version.rb
@@ -1,7 +1,7 @@
 # copied from https://github.com/rails/strong_parameters/edit/master/lib/strong_parameters/version.rb
 # I;'m copying the DHH philosophy here.
 module Storazzo
-    DHH_VERSION = "0.2.3_TODOFileRead" # TODO file.read ../../VERSION . chomp
+    DHH_VERSION = "0.2.3._TODOFileRead.1" # TODO file.read ../../VERSION . chomp
     RICC_VERSION = File.read("../VERSION").chomp
 
     public
