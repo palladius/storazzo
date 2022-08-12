a95f6310b6e58c3a3c337e888ce4a30e582befc9
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-08-12 17:40:57 +0200

    feat(bin/stats-with-md5): frequency update
    added timestamp of frequency update (so every 50 files I know how much it takes to do 50 new pics

---
+++ test/benchmark/test_hashing_functions-speed.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+# thanks https://gist.github.com/morimori/1330095 !!
+
+require 'digest/sha1'
+require 'digest/sha2'
+require 'benchmark'
+
+SRC = File.read '/dev/urandom', 1024 * 1024
+
+puts "SRC size: #{SRC.size}B"
+Benchmark.bmbm do |bm|
+  bm.report('MD5')    { 100000.times{ Digest::MD5.hexdigest    SRC } }
+  bm.report('SHA1')   { 100000.times{ Digest::SHA1.hexdigest   SRC } }
+  bm.report('SHA2')   { 100000.times{ Digest::SHA2.hexdigest   SRC } }
+  #bm.report('SHA256') { 100000.times{ Digest::SHA256.hexdigest SRC } }
+end
