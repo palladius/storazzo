808c02c8f8a297f5ac2a4cf779f3987956ecc005
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-08-14 16:38:40 +0200

    new idea does NOT work. Still adding code in case I want to explore from another computer

---
+++ test/bin/new-idea.rb
@@ -0,0 +1,17 @@
+# # include the binary in ruby
+# # like
+# require_relative '../../lib/storazzo'
+# # require 'storazzo-local'
+# puts :should_include_loads_of_includez
+
+require 'minitest/autorun'
+# require_relative '../../bin/storazzo-symlink'
+
+class BinStorazzoNewIdeaTest < Minitest::Test
+  #   # and then call main or something?
+  #   require_relative '../../bin/storazzo-symlink'
+
+  #   # and then invoke this test via rake ?
+end
+
+puts 'Doesnt work dammit.'
